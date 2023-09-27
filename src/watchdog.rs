77f004a51cf0a740dc8d4005cdf48ca1e87ba32a
fel115 <133339595+fel115@users.noreply.github.com>
2023-09-27 22:10:16 +0200

    added test function for watchdog

--- src/watchdog.rs
+++ src/watchdog.rs
@@ -24,4 +24,23 @@ impl Watchdog {
 				.map_err(|_| ModuleError::new(String::from("could not write to watchdog")))?;
 		}
 	}
+
+	//This function is only used to test if the system reboots after this module stops writing to the watchdog file.
+	//For testing change the get_background_task to test_get_background_task in main.rs
+	pub async fn test_get_background_task(
+		path: String,
+		mut interval: Interval,
+	) -> Result<Never, ModuleError> {
+		let mut handle = File::create(path)
+			.await
+			.map_err(|_| ModuleError::new(String::from("could not open watchdog")))?;
+		for _i in 1..=100 {
+			interval.tick().await;
+			handle
+				.write_all(b"a")
+				.await
+				.map_err(|_| ModuleError::new(String::from("could not write to watchdog")))?;
+		}
+		Err(ModuleError::new(String::from("doesnt run for loop")))
+	}
 }
