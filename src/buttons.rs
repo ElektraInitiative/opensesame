561d83b80d2b1c0c4ff4057116a029d3f1e98681
Markus Raab <elektra@markus-raab.org>
2023-12-05 09:50:49 +0100

    try to fix #41
    Timeout mit Sequenz [0, 15, 0, 15, 0, 15]

--- src/buttons.rs
+++ src/buttons.rs
@@ -108,7 +108,7 @@ const RELAY_LICHT_AUSSEN: u8 = 0x01 << 1;
 
 const ALL_RELAYS: u8 = RELAY_DOOR | RELAY_LICHT_AUSSEN;
 
-const PINS1_INIT: u8 = 15;
+const PINS1_INIT: u8 = 0b01100000;
 
 // board 21
 
